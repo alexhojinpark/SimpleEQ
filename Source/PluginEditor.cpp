67717bafe4af7c76cc33a0e337751496ffcee35b
alexhojinpark <hojin7737@gmail.com>
2021-11-03 18:19:30 -0700

    implemented displaying parameter value with suffix

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -74,10 +74,10 @@ void RotarySliderWithLabels::paint(juce::Graphics &g)
     
     auto sliderBounds = getSliderBounds();
     
-    g.setColour(Colours::red);
-    g.drawRect(getLocalBounds());
-    g.setColour(Colours::yellow);
-    g.drawRect(sliderBounds);
+//    g.setColour(Colours::red);
+//    g.drawRect(getLocalBounds());
+//    g.setColour(Colours::yellow);
+//    g.drawRect(sliderBounds);
     
     getLookAndFeel().drawRotarySlider(g,
                                       sliderBounds.getX(),
@@ -108,7 +108,39 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
 
 juce::String RotarySliderWithLabels::getDisplayString() const
 {
-    return juce::String(getValue());
+    if (auto* choiceParam = dynamic_cast<juce::AudioParameterChoice*>(param))
+    {
+        return choiceParam->getCurrentChoiceName();
+    }
+    juce::String str;
+    bool addK = false;
+    
+    if (auto* floatParam = dynamic_cast<juce::AudioParameterFloat*>(param))
+    {
+        float val = getValue();
+        if (val > 999.f)
+        {
+            val /= 1000.f;
+            addK = true;
+        }
+        
+        str = juce::String(val, (addK ? 2 : 0));
+        
+    }
+    else
+    {
+        jassertfalse; // this shouldnt happen
+    }
+    
+    if (suffix.isNotEmpty())
+    {
+        str << " ";
+        if (addK)
+            str << "k";
+        str << suffix;
+    }
+    
+    return str;
 }
 
 //==================================================================================
@@ -169,7 +201,6 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     auto& lowcut = monoChain.get<ChainPositions::LowCut>();
     auto& peak = monoChain.get<ChainPositions::Peak>();
     auto& highcut = monoChain.get<ChainPositions::HighCut>();
-    
     auto sampleRate = audioProcessor.getSampleRate();
     
     std::vector<double> mags;
