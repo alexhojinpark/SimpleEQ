927cc00445e36bb64f98c06a5475e6b30d4ce427
alexhojinpark <hojin7737@gmail.com>
2021-11-10 16:04:32 -0800

    analyzer button is visible

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -257,6 +257,9 @@ private:
 };
 
 //==============================================================================
+struct PowerButton : juce::ToggleButton {};
+struct AnalyzerButton : juce::ToggleButton {};
+
 /**
 */
 class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
@@ -296,7 +299,8 @@ private:
                 lowCutSlopeSliderAttachment,
                 highCutSlopeSliderAttachment;
     
-    juce::ToggleButton lowcutBypassButton, peakBypassButton, highcutBypassButton, analyzerEnabledButton;
+    PowerButton lowcutBypassButton, peakBypassButton, highcutBypassButton;
+    AnalyzerButton analyzerEnabledButton;
     
     using ButtonAttachment = APVTS::ButtonAttachment;
     ButtonAttachment lowcutBypassButtonAttachment,
